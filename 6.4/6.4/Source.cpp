ac97c7bd8d54c90b366a0a0886db942e2645324b
Chasek99 <ck121642@students.davenport.k12.ia.us>
2017-05-04 11:17:49 -0500

    update

--- 6.4/6.4/Source.cpp
+++ 6.4/6.4/Source.cpp
@@ -6,28 +6,19 @@ double getTotal(double NumbersUsed[], int numElements);
 int main()
 {
 	//declaring the array
-	int numbers[6] = { 23, 6, 47, 35, 2, 14 };
-	double Highest = 0.0;
-	double Odds = 0.0;
+	int array[6] = { 23, 6, 47, 35, 2, 14 };
+	int Highest = 0.0;
+	int Odds = 0.0;
 
-
-	for (int i = 0; i < 47;i++)
+	for (int i = 0; i < 5; i++)
 	{
-		cin >> Highest;
-		if (Highest>Odds)
-			Odds = Highest;
-		cout << "The biggest number is: " << Highest << endl;
+		if (array[i] > Highest){
+			Highest = array[i];
+		}
 	}
 
+	cout << "The highest value is:" << Highest << endl;
 
-	//calculating the total values
-	Highest = 47;
-	Odds = 0;
-
-	//displays the total and average
-	cout << "Original Numbers:"<< endl;
-	cout << "Highest Number: " << Highest << endl;
-	cout << "Odd numbers: " << Odds << endl;
 
 
 	system("pause");
