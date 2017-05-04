14791742790c8e245be2954e6b1c847c7112a9b0
Chasek99 <ck121642@students.davenport.k12.ia.us>
2017-05-04 11:26:30 -0500

    update

---
+++ 6.6/6.6/Source.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+double getTotal(double NumbersUsed[], int numElements);
+
+int main()
+{
+	//declaring the array
+	int array[6] = { 23, 6, 47, 35, 2, 14 };
+	int Odds = 0.0;
+
+	for (int i = 0; i < 6; i++)
+	
+		
+
+		
+
+
+	cout << "The Odd numbers are" << Odds << endl;
+
+
+
+	system("pause");
+	return 0;
+
+}
